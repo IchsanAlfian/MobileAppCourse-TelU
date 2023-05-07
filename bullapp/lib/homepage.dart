637514b507b428bf5f62ab4d01691bca667dd248
Daniel Suryahadi <d.suryahadi01@gmail.com>
2023-05-07 22:55:06 +0700

    Added the second page of the app, contributed by Syahdi

---
+++ bullapp/lib/homepage.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.blue[300]!,
+              Colors.blue[900]!,
+            ],
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(16),
+                width: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'BDG',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '14.00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Icon(
+                          Icons.wb_sunny,
+                          size: 72,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 16),
+                        Text(
+                          '32 C',
+                          style: TextStyle(
+                            fontSize: 48,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'CERAH',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  physics: BouncingScrollPhysics(),
+                  padding: const EdgeInsets.all(8),
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    _buildColumn(context, Icons.wb_cloudy, '14.00'),
+                    _buildColumn(context, Icons.cloud, '15.00'),
+                    _buildColumn(context, Icons.wb_cloudy, '16.00'),
+                    _buildColumn(context, Icons.wb_sunny, '17.00'),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  'Sabtu, 6 Mei 2023',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildColumn(BuildContext context, IconData iconData, String time) {
+    return Column(
+      children: [
+        Icon(
+          iconData,
+          size: 64,
+          color: Colors.white,
+        ),
+        SizedBox(height: 16),
+        Text(
+          time,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+}
